1659ff7f178c7eb7a16923595da4159dc4aca638
ShamithaSilva <shamithadevinda@gmail.com>
2018-10-02 20:46:23 +0100

    Deleting form ui

--- src/simulator/demos/Demo.java
+++ src/simulator/demos/Demo.java
@@ -1,22 +1,10 @@
 package simulator.demos;
 
-import simulator.ui.SimulatorUI;
-
-import javax.swing.*;
-import java.awt.*;
-
 public class Demo
 {
 
         public static void main(String[] args) {
-            JFrame frame = new JFrame("SimulatorUI");
-            SimulatorUI ui = new SimulatorUI();
-            JPanel panel = ui.getMainPanel();
-            panel.setPreferredSize(new Dimension(1200,650));
-            frame.setContentPane(panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setVisible(true);
+            
         }
 
 }
