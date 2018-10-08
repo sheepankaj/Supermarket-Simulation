b7e8e0be2cf9047b9c09fa31469fcc3247cd2344
ShamithaSilva <shamithadevinda@gmail.com>
2018-10-08 11:44:31 +0100

    Changing UI for Stats

--- src/simulator/demos/Demo.java
+++ src/simulator/demos/Demo.java
@@ -2,7 +2,7 @@ package simulator.demos;
 
 import simulator.ui.SimulatorUI;
 
-public class Demo {
+class Demo {
 	public static void main(String[] args) {
 		SimulatorUI ui = new SimulatorUI();
 		ui.setVisible(true);
--- src/simulator/ui/SimulatorUI.java
+++ src/simulator/ui/SimulatorUI.java
@@ -1,30 +1,30 @@
 package simulator.ui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.GridBagLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
-import javax.swing.UIManager;
 import java.awt.Color;
-import java.awt.FlowLayout;
+
 import javax.swing.BoxLayout;
-import javax.swing.JSlider;
-import javax.swing.JTable;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+import javax.swing.JTextField;
 
 public class SimulatorUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txTotWaitCustomer;
+	private JTextField txTotUtiForCheckout;
+	private JTextField txTotProductsProcessed;
+	private JTextField txAvgCustomerWaitTime;
+	private JTextField textField;
+	private JTextField txAvgProductsPerTrolley;
+	private JTextField txNoOfLostCustomers;
 	/**
 	 * Create the frame.
 	 */
@@ -44,48 +44,99 @@ public class SimulatorUI extends JFrame {
 		checkOuts.setLayout(new BoxLayout(checkOuts, BoxLayout.Y_AXIS));
 		
 		JPanel parameters = new JPanel();
-		parameters.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Parameters", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		parameters.setBounds(33, 278, 399, 199);
+		parameters.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Statistics", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		parameters.setBounds(22, 25, 343, 209);
 		contentPane.add(parameters);
 		
-		JLabel lbProdPerTrolley = new JLabel("Products per Trolley");
+		JLabel lbTotWaitCustomer = new JLabel("Total wait time for Customer");
 		
-		JSlider slProdPerTrolley = new JSlider();
-		slProdPerTrolley.setMinorTickSpacing(10);
-		slProdPerTrolley.setMajorTickSpacing(50);
-		slProdPerTrolley.setPaintLabels(true);
-		slProdPerTrolley.setPaintTicks(true);
-		slProdPerTrolley.setMaximum(200);
+		JLabel lbTotUtiForCheckout = new JLabel("Total Utilization for Checkout");
 		
-		JLabel lbScanTime = new JLabel("Scan Time");
+		txTotWaitCustomer = new JTextField();
+		txTotWaitCustomer.setColumns(10);
 		
-		JSlider slScanTime = new JSlider();
-		slScanTime.setMaximum(6);
+		txTotUtiForCheckout = new JTextField();
+		txTotUtiForCheckout.setColumns(10);
+		
+		JLabel lbTotProductsProcessed = new JLabel("Total Products Processed");
+		
+		txTotProductsProcessed = new JTextField();
+		txTotProductsProcessed.setColumns(10);
+		
+		JLabel lbAvgCustomerWaitTime = new JLabel("Avg Customer wait time");
+		
+		txAvgCustomerWaitTime = new JTextField();
+		txAvgCustomerWaitTime.setColumns(10);
+		
+		JLabel lbCheckoutUtilization = new JLabel("Avg Checkout Utilization");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lbAvgProductsPerTrolley = new JLabel("Avg Products per trolley");
+		
+		txAvgProductsPerTrolley = new JTextField();
+		txAvgProductsPerTrolley.setColumns(10);
+		
+		JLabel lbNoOfLostCustomers = new JLabel("No Of  lost Customers");
+		
+		txNoOfLostCustomers = new JTextField();
+		txNoOfLostCustomers.setColumns(10);
 		GroupLayout gl_parameters = new GroupLayout(parameters);
 		gl_parameters.setHorizontalGroup(
 			gl_parameters.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_parameters.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_parameters.createParallelGroup(Alignment.LEADING)
-						.addComponent(lbProdPerTrolley)
-						.addComponent(lbScanTime))
-					.addPreferredGap(ComponentPlacement.RELATED, 71, Short.MAX_VALUE)
-					.addGroup(gl_parameters.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(slScanTime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(slProdPerTrolley, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
+						.addComponent(lbTotWaitCustomer)
+						.addComponent(lbTotUtiForCheckout)
+						.addComponent(lbTotProductsProcessed)
+						.addComponent(lbAvgCustomerWaitTime)
+						.addComponent(lbCheckoutUtilization)
+						.addComponent(lbAvgProductsPerTrolley)
+						.addComponent(lbNoOfLostCustomers))
+					.addGap(38)
+					.addGroup(gl_parameters.createParallelGroup(Alignment.LEADING)
+						.addComponent(txNoOfLostCustomers, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txAvgProductsPerTrolley, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txAvgCustomerWaitTime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txTotProductsProcessed, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txTotUtiForCheckout, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txTotWaitCustomer, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(115, Short.MAX_VALUE))
 		);
 		gl_parameters.setVerticalGroup(
 			gl_parameters.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_parameters.createSequentialGroup()
-					.addGroup(gl_parameters.createParallelGroup(Alignment.LEADING)
-						.addComponent(slProdPerTrolley, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lbProdPerTrolley))
+					.addGroup(gl_parameters.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbTotWaitCustomer)
+						.addComponent(txTotWaitCustomer, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_parameters.createParallelGroup(Alignment.LEADING)
-						.addComponent(slScanTime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lbScanTime))
-					.addContainerGap(118, Short.MAX_VALUE))
+					.addGroup(gl_parameters.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbTotUtiForCheckout)
+						.addComponent(txTotUtiForCheckout, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_parameters.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lbTotProductsProcessed)
+						.addComponent(txTotProductsProcessed, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_parameters.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lbAvgCustomerWaitTime)
+						.addComponent(txAvgCustomerWaitTime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_parameters.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbCheckoutUtilization)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_parameters.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbAvgProductsPerTrolley)
+						.addComponent(txAvgProductsPerTrolley, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_parameters.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lbNoOfLostCustomers)
+						.addComponent(txNoOfLostCustomers, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(23, Short.MAX_VALUE))
 		);
 		parameters.setLayout(gl_parameters);
 	}
